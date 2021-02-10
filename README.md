5f41e4549f8f4ddaa88177fc5a575fb859263ef0
nsde <neostyxde@gmail.com>
2021-02-10 14:48:17 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # discord-coinbot
 A coin system for Discord with an API.
+
+Basically an updated and improved version of **https://github.com/nsde/SCD-Bot**.
+
+*"Why didn't I just update the SCD-Repo?"*, you might ask.
+> It's because the SCD-Bot was created for a gaming clan, but thgis one is a public bot.
+
+*"Are you going to update the SCD-Repo anytime soon?"*
+
+> I'm not sure yet, but probably no.
+
+# Installation
+Execute in root dir: `pip install -r requirements.txt` to install the needed libaries.
+After that, create a file in "\config" called "token.txt". The content of the file has to be the bot token (see the section "Security" > "Token") for security information.
+
+## Fix errors:
+If you get "ERROR: Could not build wheels for multidict, yarl which use PEP 517 and cannot be installed directly":
+- __(not recommended)__ Downgrade to Python 3.8.6 
+- __(not recommended)__ Download the "cp39" (for Python 3.9)-Versions of:
+
+    -**lfd.uci.edu/~gohlke/pythonlibs/#yarl**
+
+    -**lfd.uci.edu/~gohlke/pythonlibs/#multidict**
+
+    remember to choose the right win_amd64/win32-Version.
+
+    And do `pip install `**FILENAME** (e.g *yarl-1.6.3-cp39-cp39-win_amd64.whl*)` --force-reinstall` in the directory you downloaded the files for **both the two files**. 
+
+    Then try `pip install discord.py` or `pip install discord` again.
+
+- __(recommended/best way)__ install **https://visualstudio.microsoft.com/visual-cpp-build-tools/** (this worked for me)
+
+- alternatively, see: https://stackoverflow.com/a/64861883
+
+Run the "main"-python file in /src and follow the instructions and you are *good to go*!
+
+
+# Security note
+This section will inform you about how you can safe your bot against black hat hackers/exploits etc.
+
+## Permissions
+Please go to the Discord Developer Portal `/applications/<application id>/bot` and scroll down to the section "Bot permissions". From there, tick all **needed and used** permissions. Do NOT tick "Administrator" - this will allow the bot to do almost everything on your server. And, do NOT tick unused permissions.
+
+## Token
+The bot token is secret! Everyone with access to the token can execute everything allowed to the bot (see "Permissions"). Therefore, remember to put the token.txt in the `.gitignore`, if you are using Git.
--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,112 @@
-import discord
-from discord.ext import commands
+# Load modules
+try:
+    import os
+    import sys
+    import json
+    import yaml
+    import random
+    import discord
 
-TOKEN = "HIER DEN TOKEN EINFÜGEN"
+    from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!') # Command-Symbol
+except:
+    print("Couldn't load all the libaries. Please install the libaries listed in <root>/requirements.txt.")
+    sys.exit(0)
 
-@bot.command(name='hallo')
-async def halloSagen(ctx): # auf den Befehl "hören"
-    await ctx.send("Hallo, du!")
+# Some basic defenitions
+CWD = os.getcwd()
+TOKEN = open(CWD + "\\config\\token.txt").read()
+print(TOKEN)
 
-@bot.command(name='bye')
-async def byeSagen(ctx): # auf den Befehl "hören"
-    await ctx.send("Bis dann!")
+# Set token
+def tokenError():
+    print("Please type in a valid bot token.\n\nThe token can be found at config/token.txt. Remember to not give anyone access to this secret file!")
+    set_token = input("Token: ")
+    with open (CWD + "\\config\\token.txt", "w") as f:
+        token = f.write(set_token)
 
-client.run(TOKEN) # Bot starten
+try:
+    with open (CWD + "\\config\\token.txt") as f:
+        token = f.read()
+    print("Token loaded. Length: " + str(len(token)))
+
+    if token == "":
+        tokenError()
+
+except:
+    tokenError()
+
+# Load config
+with open(CWD + '\\config\\config.yml') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+    def fixuml(x):
+        """Fix wrong coding of äüö (German language only)"""
+        return x.replace("Ã¤","ä").replace("Ã¶","ö").replace("Ã¼", "ü")
+
+    def lang(x):
+        return fixuml(random.choice(config["language"][x]))
+
+    print(config)        
+
+    # Def bot
+    client = commands.Bot(command_prefix = config["main"]["prefix"])
+
+with open(CWD + '\\data\\dailycoins.yml') as f:
+    daily = yaml.load(f, Loader=yaml.FullLoader)
+
+# Bot
+@client.event
+async def on_ready():
+    print(f'\nLogged in as {client.user}\n')
+
+
+@client.command(name='ping')
+async def ping(ctx):
+    await ctx.send('Ping!')
+
+
+@client.command(name='say')
+async def say(ctx, *args):
+    var = ''
+    for s in args:
+        var += f'{s} '
+    await ctx.send(var)
+
+
+@client.command(name='user')
+async def user(ctx):
+    user = ctx.message.author
+    mention = ctx.message.author.mention
+    id = ctx.message.author.id
+    await ctx.send(user)
+    await ctx.send(mention)
+    await ctx.send(id)
+
+
+@client.command(name='stop')
+@commands.has_permissions(administrator=True)
+async def stop_bot(ctx):
+    await ctx.send('Bot stoppt...')
+    await client.close()
+
+# Commands
+@client.command(name='dailycoins', aliases=["dcoins"])
+async def dailycoins(ctx):
+    if ctx.message.author not in daily["blocked"]["coins"]:
+        await ctx.send(lang("dailycoins") + "\n> +" + str(random.randint(config["currency"]["rarity_normal"]["min"], config["currency"]["rarity_normal"]["max"])) + " " + config["currency"]["symbols"]["currency_normal"])
+        daily["blocked"]["coins"].append(ctx.message.author)
+        with open(CWD + '\\config\\daily.yml', 'w') as f:
+            daily_list = []
+            daily_list.append(daily)
+            yaml.dump(daily_list, f)
+    else:
+        await ctx.send(config["currency"]["symbols"]["error"] + " " + lang("dailyerror"))
+
+# Run
+try:
+    client.run(token)
+
+except:
+    print("ERROR: Unable to run the client. Did you input a invalid token?")
+    sys.exit(0)
